a2e5fc395af0d200c3cb8a570a941042bc03e20d
Amyy0404 <anyvv9@gmail.com>
2024-04-27 13:26:57 +0200

    Colour Changes

--- Scripts/main-menu.js
+++ Scripts/main-menu.js
@@ -2,8 +2,8 @@ const menuItems = [
     {name:"HOME", href: "index.html"},
     {name:"BLOG POSTS", href: "blogs.html"},   
     {name:"DESIGNS", href: "designs.html"},
-    {name:"PORTFOLIO", href: "portfolio.html"},
     {name:"ESSAYS", href: "essays.html"},
+    {name:"PORTFOLIO", href: "portfolio.html"},  
     {name:"ABOUT ME", href: "about.html"},
 ]
 
