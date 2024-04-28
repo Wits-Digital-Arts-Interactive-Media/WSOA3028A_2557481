52ff0d54ffc3080ee7907ec962cc43e0f2901a80
Amyy0404 <anyvv9@gmail.com>
2024-04-28 23:04:14 +0200

    colour palette

---
+++ Scripts/colour-palette.js
@@ -0,0 +1,19 @@
+
+const originalColor = "rgb(59,57,53)";
+
+function changeBackgroundColor() {
+    const colors = [
+        "rgba(200,194,172,255)",
+        "rgba(146,156,145,255)", 
+        "rgba(159,139,130,255)", 
+        "rgba(92,98,110,255)", 
+        "rgba(56,56,56,255)"];
+
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    document.body.style.backgroundColor = randomColor;
+}
+
+function resetBackgroundColor() {
+    document.body.style.backgroundColor = originalColor;
+}
