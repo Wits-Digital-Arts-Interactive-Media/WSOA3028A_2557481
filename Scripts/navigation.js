e3be5905e908c594ee3d3de52e6ea787f66f992e
Amyy0404 <anyvv9@gmail.com>
2024-05-26 20:49:58 +0200

    nav css

--- Scripts/navigation.js
+++ Scripts/navigation.js
@@ -7,18 +7,23 @@ const navLinks = [
     { text: 'ABOUT ME', url: '../about/about.html' }
 ];
 
-
 function createNavigationBar() {
     const navElement = document.querySelector('nav');
     if (!navElement) return; 
 
     const navList = document.createElement('ul');
+    const currentPage = window.location.pathname.split("/").pop();
 
     navLinks.forEach(link => {
         const listItem = document.createElement('li');
         const anchor = document.createElement('a');
         anchor.textContent = link.text;
         anchor.href = link.url;
+
+        if (link.url.includes(currentPage)) {
+            anchor.classList.add('active');
+        }
+
         listItem.appendChild(anchor);
         navList.appendChild(listItem);
     });
